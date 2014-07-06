cd36a6faf968b31384f6af193bf76d496174e95e
AjayBarot-BTC <ajay.barot@beaverlogic.com>
2014-07-06 20:41:42 +0530

    modified readme file

---
+++ README.md
@@ -0,0 +1,5 @@
+https://github.com/yrgoldteeth/css3-progress-bar-rails 
+
+and using sidekick 
+
+http://www.sunwooyang.com/posts/3
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Progressbar::Application.config.secret_key_base = '64e7dd144f1370fbc93974218d78a397b46e6cdf69c0e0b202e7be91a7e1df75990f7b575d7544c09be77ada5f805f1b296f004c0f0b6ecab9249eca9b3122ac'
